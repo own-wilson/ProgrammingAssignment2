c2c69a573fcdd150f8978ce9fa476ec5d7ceb5f2
Own-Wilson <graham.nolen@gmail.com>
2022-10-06 17:09:00 -0400

    final commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## creates or populates a special matrix depending 
+## on passed parameters, and defines them in the 
+## global environment
+## defines functions to get and set 
+## variables for the matrix passed to it
+## Returns a list of the defined variables
 
 makeCacheMatrix <- function(x = matrix()) 
 {
@@ -20,11 +26,16 @@ makeCacheMatrix <- function(x = matrix())
 }
 
 
-## Write a short comment describing this function
+## Takes an invertable matrix
+## If the inversion of the matrix 
+## has already been defined, the function 
+## returns the inversion
+## Otherwise, the function calculates the inversion,
+## and returns that inverted matrix.
 
 cacheSolve <- function(x, ...) 
 {
-  ## Return a matrix that is the inverse of 'x'
+  
   inv <- x$getinverse()
   if(!is.null(inv))
   {
@@ -33,5 +44,6 @@ cacheSolve <- function(x, ...)
   myData <- x$get()
   inv <- solve(myData, ...)
   x$setinverse(inv)
+  ## Return a matrix that is the inverse of 'x'
   inv
 }
